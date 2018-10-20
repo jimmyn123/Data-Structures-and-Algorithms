41b8d6c2b3b79ae065cb1a4f52edcad06f2ae0f5
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-19 22:53:15 -0700

    Added to main and then printed out + commented

--- Challenges/ReverseLinkedList/ReverseLinkedList/Program.cs
+++ Challenges/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -8,16 +8,28 @@ namespace ReverseLinkedList
     {
         static void Main(string[] args)
         {
+            // creates linked list with values 1 -> 2 -> 3 -> 4 ->5
             LList input = new LList(new Node(5));
             input.Add(new Node(4));
             input.Add(new Node(3));
             input.Add(new Node(2));
             input.Add(new Node(1));
 
+            // prints out linked list before reverse
+            WriteLine("Array before reverse: ");
             input.Print();
+
+            // prints out linked list after reverse
+            WriteLine("Array after reverse: ");
             Reverse(input);
+            input.Print();
         }
 
+        /// <summary>
+        /// Reverse method for linked list arrays
+        /// </summary>
+        /// <param name="input">The linked list to reverse</param>
+        /// <returns>The reversed linked list</returns>
         public static LList Reverse(LList input)
         {
             Node prev = null;
@@ -30,7 +42,7 @@ namespace ReverseLinkedList
                 prev = curr;
                 curr = nextCurr;
             }
-
+            input.Head = prev;
             return input;
         }
     }
