f958ae6bc47c519b680a8bac10c30ddf09ec4f9b
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-19 22:40:57 -0700

    set up structure, added linked lists to main

---
+++ Challenges/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Day06_LinkedList.Classes;
+
+namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LList input = new LList(new Node(5));
+            input.Add(4);
+            input.Add(3);
+            input.Add(2);
+            input.Add(1);
+            Reverse(input);
+        }
+
+        public node Reverse(LList)
+        {
+
+        }
+    }
+}
