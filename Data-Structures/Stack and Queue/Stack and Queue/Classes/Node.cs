2eedc5fb4e4e3f20e07ad18d3377aaa509fd8135
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-22 09:54:48 -0700

    More comments

--- Data-Structures/Stack and Queue/Stack and Queue/Classes/Node.cs
+++ Data-Structures/Stack and Queue/Stack and Queue/Classes/Node.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection.Metadata;
-using System.Text;
-
-namespace Stack_and_Queue.Classes
+﻿namespace Stack_and_Queue.Classes
 {
     public class Node
     {
         public object Value { get; set; }
         public Node Next { get; set; }
 
+        /// <summary>
+        /// Constructor, makes a new node
+        /// </summary>
+        /// <param name="value">Value of new node</param>
         public Node(object value)
         {
             Value = value;
--- Data-Structures/Stack and Queue/Stack and Queue/Classes/Queue.cs
+++ Data-Structures/Stack and Queue/Stack and Queue/Classes/Queue.cs
@@ -5,26 +5,40 @@
         public Node Front { get; set; }
         public Node Rear { get; set; }
 
+        /// <summary>
+        /// Creates a new Queue with the node
+        /// </summary>
+        /// <param name="node">the new node value</param>
         public Queue(Node node)
         {
+            // sets front and rear to the node that was passed in
             Front = node;
             Rear = node;
         }
 
-        // Peek
+        /// <summary>
+        /// The peek, shows what node will be de-queue
+        /// </summary>
+        /// <returns>returns the front node</returns>
         public Node Peek()
         {
             return Front;
         }
 
-        // Enqueue
+        /// <summary>
+        /// Adds a new node to the end of the line
+        /// </summary>
+        /// <param name="node">The node to put placed at the rear</param>
         public void Enqueue(Node node)
         {
             Rear.Next = node;
             Rear = node;
         }
 
-        // Dequeue
+        /// <summary>
+        /// The node that gets removed from the front of the queue
+        /// </summary>
+        /// <returns>The node that was de-queued</returns>
         public Node Dequeue()
         {
             Node temp = Front;
