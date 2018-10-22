3ea953a7cd61a90c26a50738873b884f841be25b
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-22 10:15:33 -0700

    Added push and pop test.

--- Data-Structures/Stack and Queue/StackAndQueueTest/StackAndQueueTest.cs
+++ Data-Structures/Stack and Queue/StackAndQueueTest/StackAndQueueTest.cs
@@ -6,12 +6,22 @@ namespace StackAndQueueTest
 {
     public class StackAndQueueTest
     {
+        // Tests the push for a stack
         [Fact]
         public void PushNodeOnStackTest()
         {
             Stack st = new Stack(new Node(7));
             st.Push(new Node(50));
             Assert.True((int)st.Peek().Value == 50);
+        }
+        // Tests the Pop for a stack
+        [Fact]
+        public void PopNodeOnStackTest()
+        {
+            Stack st = new Stack(new Node(7));
+            st.Push(new Node(50));
+            st.Pop();
+            Assert.True((int)st.Peek().Value == 7);
         }
     }
 }
