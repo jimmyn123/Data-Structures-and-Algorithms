6be15adf1c845577746366c194d21b4eb51fac0d
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-22 09:56:28 -0700

    Finished commenting

--- Data-Structures/Stack and Queue/Stack and Queue/Classes/Stack.cs
+++ Data-Structures/Stack and Queue/Stack and Queue/Classes/Stack.cs
@@ -8,19 +8,29 @@ namespace Stack_and_Queue.Classes
     {
         public Node Top { get; set; }
 
+        /// <summary>
+        /// Stack constructor
+        /// </summary>
+        /// <param name="node">The top stack to start</param>
         public Stack(Node node)
         {
             Top = node;
         }
 
-        //Peek
+        /// <summary>
+        /// The peek method, returns what will be popped
+        /// </summary>
+        /// <returns>The node that will be popped</returns>
         public Node Peek()
         {
             // this is a great place for a try/catch (if you want)
             return Top;
         }
 
-        //Pop
+        /// <summary>
+        /// Pops the top node
+        /// </summary>
+        /// <returns>Returns the top none and removes from stack</returns>
         public Node Pop()
         {
             Node temp = Peek();
@@ -28,7 +38,11 @@ namespace Stack_and_Queue.Classes
             temp.Next = null;
             return temp;
         }
-        //Push
+
+        /// <summary>
+        /// Adds a new node at the top
+        /// </summary>
+        /// <param name="node">The node that is added</param>
         public void Push(Node node)
         {
             node.Next = Top;
