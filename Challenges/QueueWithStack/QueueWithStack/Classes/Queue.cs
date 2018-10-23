db009537053cce4bee4af7188f4285dd6fc38490
jimmyn123 <jimmyn123@gmail.com>
2018-10-22 18:26:43 -0700

    finished main, fixed stack so it would return null properly.

--- Challenges/QueueWithStack/QueueWithStack/Classes/Queue.cs
+++ Challenges/QueueWithStack/QueueWithStack/Classes/Queue.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Text;
-using Stack_and_Queue.Classes;
 
 namespace QueueWithStack.Classes
 {
@@ -19,27 +18,23 @@ namespace QueueWithStack.Classes
         public void Enqueue(Node input)
         {
             rear.Push(input);
-            rear.Size++;
         }
 
         public Node Dequeue()
         {
-            int size = rear.Size;
-            while(size > 0)
+            while(rear.Size > 0)
             {
                 front.Push(rear.Pop());
-                size--;
             }
 
             Node result = front.Pop();
-            size = --rear.Size;
             
-            while(size > 0)
+            while(front.Size > 0)
             {
                 rear.Push(front.Pop());
             }
 
-            return result;   
+            return result;
         }
     }
 }
--- Challenges/QueueWithStack/QueueWithStack/Program.cs
+++ Challenges/QueueWithStack/QueueWithStack/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using static System.Console;
-using Stack_and_Queue.Classes;
+using QueueWithStack.Classes;
 
 namespace QueueWithStack
 {
@@ -11,6 +11,21 @@ namespace QueueWithStack
             WriteLine("Adding 1,2,3 in that order");
             Queue q = new Queue();
 
+            q.Enqueue(new Node(1));
+            q.Enqueue(new Node(2));
+            q.Enqueue(new Node(3));
+
+            WriteLine("Dequeueing last node added...");
+            WriteLine("Node dequeued: " + (int)q.Dequeue().Value);
+
+            WriteLine("Dequeueing last node added...");
+            WriteLine("Node dequeued: " + (int)q.Dequeue().Value);
+
+            WriteLine("Dequeueing last node added...");
+            WriteLine("Node dequeued: " + (int)q.Dequeue().Value);
+
+            WriteLine("Dequeueing last node added...");
+            WriteLine("Node dequeued: " + (int)q.Dequeue().Value);
         }
     }
 }
