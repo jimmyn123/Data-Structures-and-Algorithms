8ff4e28a11b02aa44589ce91186f7a5651266ae1
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-23 23:48:55 -0700

    commented and created main method

--- Challenges/fifo_animal_shelter/fifo_animal_shelter/Class/AnimalShelter.cs
+++ Challenges/fifo_animal_shelter/fifo_animal_shelter/Class/AnimalShelter.cs
@@ -15,66 +15,97 @@ namespace fifo_animal_shelter.Class
         /// <summary>
         /// Adds a node to the rear of the shelter
         /// </summary>
-        /// <param name="newNode"></param>
+        /// <param name="newNode">The node to be added</param>
         public void Enqueue(Node newNode)
         {
+            // if empty list, set front to the new node
             if(Front == null)
             {
                 Front = newNode;
             }
             else
             {
+                // otherwise set the new rear
                 Rear.Next = newNode;
             }
             Rear = newNode;
             Size++;
         }
 
+        /// <summary>
+        /// Dequeues a node from the animal shelter
+        /// </summary>
+        /// <param name="pref">The type of animal to dequeue</param>
+        /// <returns>Returns the node of the first animal that matches the type</returns>
         public Node Dequeue(string pref)
         {
+            // set current node to front
             Node curr = Front;
             Node prev = null;
-            Node result = Front;
+
             int counter = 0;
+            // while the counter is less than the size
             while (counter < Size)
             {
+                // if the animal is the same as the pref
                 if (((Animal)curr.Value).Name.Equals(pref.ToLower()))
                 {
+                    // check to see if there is a previous node
                     if (prev != null)
                     {
+                        // set the previous node's next node to the next node
                         prev.Next = curr.Next;
                     } else
                     {
+                        // if not then front has to be reset
                         Front = curr.Next;
                     }
+
+                    // check to see if the current has a next
                     if(curr.Next == null)
                     {
+                        // if not, then set the rear
                         Rear = prev;
                     }
+
+                    // reset the next to null
                     curr.Next = null;
                     Size--;
+
+                    // return the node
                     return curr;
                 }
+
+                // increment to the next nodes and counter
                 prev = curr;
                 curr = curr.Next;
                 counter++;
             }
+
+            // resets to the first node to return
+            curr = Front;
             Front = Front.Next;
-            result.Next = null;
+            curr.Next = null;
             Size--;
-            return result;
+            return curr;
         }
 
+        /// <summary>
+        /// Prints out all nodes in the shelter
+        /// </summary>
         public void Print()
         {
+            // set current node
             Node curr = Front;
             
             int counter = 0;
+
+            // loop through and print
             while(counter < Size)
             {
                 Console.WriteLine("Shelter has a " + ((Animal)curr.Value).Name);
-            curr = curr.Next;
-            counter++;
+                curr = curr.Next;
+                counter++;
             }
         }
     }
--- Challenges/fifo_animal_shelter/fifo_animal_shelter/Program.cs
+++ Challenges/fifo_animal_shelter/fifo_animal_shelter/Program.cs
@@ -8,8 +8,17 @@ namespace fifo_animal_shelter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FIFOAnimalShelter();
 
+
+        }
+
+        /// <summary>
+        /// Adds dogs and cats into an animalshelter and then removes them
+        /// </summary>
+        public static void FIFOAnimalShelter()
+        {
+            // creates new animal shelter and dogs and cats and puts them in
             AnimalShelter shelter = new AnimalShelter();
             shelter.Print();
             shelter.Enqueue(new Node(new Dog()));
@@ -18,11 +27,25 @@ namespace fifo_animal_shelter
             shelter.Enqueue(new Node(new Dog()));
             shelter.Enqueue(new Node(new Cat()));
 
+            // prints initial shelter after creating them
             shelter.Print();
 
-            WriteLine("Dequeueing...");
+            // dequeue a cat
+            WriteLine("Dequeueing...cat");
             shelter.Dequeue("Cat");
             shelter.Print();
+
+            // dequeue second cat
+            WriteLine("Dequeueing...cat");
+            shelter.Dequeue("Cat");
+            shelter.Print();
+
+            // dequeue a third cat, but ends up w/ a dog because no more cats
+            WriteLine("Dequeueing...cat");
+            shelter.Dequeue("Cat");
+            shelter.Print();
+
+            WriteLine("No more cats so it dequeued a dog");
         }
     }
 }
