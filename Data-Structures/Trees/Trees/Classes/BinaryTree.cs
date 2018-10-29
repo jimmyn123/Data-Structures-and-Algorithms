97b1185d41daaf4d3ebbfb003b3caaa6560709df
jimmyn123 <jimmyn123@gmail.com>
2018-10-29 11:00:57 -0700

    added binary tree, nodes, and main

---
+++ Data-Structures/Trees/Trees/Classes/BinaryTree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public List<Node> returnList;
+        
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public List<Node> PreOrder()
+        {
+            returnList = new List<Node>();
+            PreOrderRecursive(Root);
+            return returnList;
+        }
+
+        public void PreOrderRecursive(Node root)
+        {
+            Console.WriteLine((int)root.Value);
+            returnList.Add(Root);
+
+            if (root.Left != null)
+            {
+                PreOrderRecursive(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                PreOrderRecursive(root.Right);
+            }
+        }
+
+        public List<Node> InOrder()
+        {
+            returnList = new List<Node>();
+            InOrderRecursive(Root);
+            return returnList;
+        }
+
+        public void InOrderRecursive(Node root)
+        {
+            if (root.Left != null)
+            {
+                PreOrderRecursive(root.Left);
+            }
+
+            Console.WriteLine((int)root.Value);
+            returnList.Add(root);
+
+            if (root.Right != null)
+            {
+                PreOrderRecursive(root.Right);
+            }
+        }
+
+        public List<Node> PostOrder()
+        {
+            returnList = new List<Node>();
+            PostOrderRecursive(Root);
+            return returnList;
+        }
+
+        public void PostOrderRecursive(Node root)
+        {
+            if (root.Left != null)
+            {
+                PreOrderRecursive(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                PreOrderRecursive(root.Right);
+            }
+
+            Console.WriteLine((int)root.Value);
+            returnList.Add(root);
+        }
+    }
+}
---
+++ Data-Structures/Trees/Trees/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Trees.Classes;
+using static System.Console;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create the tree
+            Node root = new Node(1);
+            Node leftChild = new Node(2);
+            Node rightChild = new Node(3);
+            Node LeftChildLeft = new Node(4);
+            Node LeftChildRight = new Node(5);
+            Node RightChildLeft = new Node(6);
+            Node RightChildRight = new Node(7);
+            root.Left = leftChild;
+            root.Right = rightChild;
+            leftChild.Left = LeftChildLeft;
+            leftChild.Right = LeftChildRight;
+            rightChild.Left = RightChildLeft;
+            rightChild.Right = RightChildRight;
+
+            BinaryTree bt = new BinaryTree(root);
+
+            // Prints preorder
+            WriteLine("======== Pre-order ========");
+            bt.PreOrder();
+            WriteLine("======== In-order ========");
+            bt.InOrder();
+            WriteLine("======== Post-order ========");
+            bt.PostOrder();
+        }
+    }
+}
