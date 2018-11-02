653a99a9cb6e9b28081fbfd88adb4a0d64c6ef16
Jimmy Nguyen <jimmyn123@gmail.com>
2018-11-01 19:56:17 -0700

    Had to re-do all commits into one. sad.

---
+++ Challenges/BreadthFirst/BreadthFirst/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Trees.Classes;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace BreadthFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // created node and attached children.
+            Node root = new Node(1);
+            Node leftChild = new Node(2);
+            Node rightChild = new Node(3);
+            Node leftChildLeftChild = new Node(4);
+            Node leftChildRightChild = new Node(5);
+            Node rightChildLeftChild = new Node(6);
+            Node rightChildRightChild = new Node(7);
+            root.Left = leftChild;
+            root.Right = rightChild;
+            leftChild.Left = leftChildLeftChild;
+            leftChild.Right = leftChildRightChild;
+            rightChild.Left = rightChildLeftChild;
+            rightChild.Right = rightChildRightChild;
+
+            WriteLine("added values, 1, 2, 3, 4, 5, 6, 7 to tree. Should display the same.");
+
+            // created binary tree abd called methods
+            BinaryTree bt = new BinaryTree(root);
+            BreadthFirst(bt);
+
+        }
+
+        /// <summary>
+        /// Does a breadth first traversal over binary tree
+        /// </summary>
+        /// <param name="bt">the binary tree</param>
+        public static void BreadthFirst(BinaryTree bt)
+        {
+            // creats a new queue and adds the initial root node
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(bt.Root);
+
+            // while the q is not empty
+            while (q.Peek() != null)
+            {
+                // dequeue the root and then print the value and enqueue children.
+                Node root = q.Dequeue();
+                q.Enqueue(root.Left);
+                q.Enqueue(root.Right);
+
+                WriteLine(root.Value);
+            }
+        }
+    }
+}
---
+++ Challenges/BreadthFirst/README.md
@@ -0,0 +1,15 @@
+# Breadth First Traversal
+A breadth first traversal of a binary tree
+
+## Challenge
+Given a binary tree, do a breadth first traversal of the tree and write it out to console
+
+## Approach & Efficiency
+
+Big O time: O(n)
+You have to iterate through every element in the binary tree. That is n nodes, therefore time is O(n)
+Big O space: O(1)
+We aren't creating any new objects. We are using the same nodes we had before therefore space is O(1).
+
+## Solution
+![Mult-bracket validation whiteboard picture](../../assets/breadth_first.jpg).
