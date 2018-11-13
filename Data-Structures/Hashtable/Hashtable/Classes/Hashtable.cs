7730ec78cc28505cd0d76b5cc9fe32bcb74c47a1
jimmyn123 <jimmyn123@gmail.com>
2018-11-13 14:14:16 -0800

    added hashtable ds, added gethash function

---
+++ Data-Structures/Hashtable/Hashtable/Classes/Hashtable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtable.Classes
+{
+    class Hashtable
+    {
+        public LinkedList<Node>[] HashTableArray { get; set; }
+
+        public Hashtable() : this(13)
+        {
+        }
+
+        public Hashtable(int buckets)
+        {
+            HashTableArray = new LinkedList<Node>[buckets];
+        }
+
+        public int GetHash(string key)
+        {
+            int index = 0;
+            foreach(char c in key)
+            {
+                index += (int)c;   
+            }
+
+            index *= 137;
+            index %= HashTableArray.Length;
+            return index;
+        }
+
+        public void Add(string key, Node input)
+        {
+            int index = GetHash(key);
+            if(HashTableArray[index] == null)
+            {
+                LinkedList<Node> stored = new LinkedList<Node>();
+                stored.AddLast(input);
+                HashTableArray[index] = stored;
+            }
+            else
+            {
+                LinkedList<Node> stored = HashTableArray[index];
+                foreach(Node curr in )
+            }
+        }
+    }
+}
---
+++ Data-Structures/Hashtable/Hashtable/Classes/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtable.Classes
+{
+    class Node
+    {
+        public object Key { get; set; }
+        public object Value { get; set; }
+    }
+}
