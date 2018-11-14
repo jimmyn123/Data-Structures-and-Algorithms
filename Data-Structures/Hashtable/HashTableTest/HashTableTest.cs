f2424e1ff8d78e7935a3e566069c416065337a7e
jimmyn123 <jimmyn123@gmail.com>
2018-11-13 18:14:56 -0800

    finished tests

--- Data-Structures/Hashtable/HashTableTest/HashTableTest.cs
+++ Data-Structures/Hashtable/HashTableTest/HashTableTest.cs
@@ -6,6 +6,11 @@ namespace HashTableTest
 {
     public class HashTableTest
     {
+        /// <summary>
+        /// Tests adding to the hashtable
+        /// </summary>
+        /// <param name="key">the key to add</param>
+        /// <param name="value">the value to add</param>
         [Theory]
         [InlineData("First", 5)]
         [InlineData("Second", 13)]
@@ -18,6 +23,11 @@ namespace HashTableTest
             Assert.True(ht.Contains(key));
         }
 
+        /// <summary>
+        /// Tests the contains method
+        /// </summary>
+        /// <param name="key">the key to find</param>
+        /// <param name="found">the expected result</param>
         [Theory]
         [InlineData("Cat", true)]
         [InlineData("Dog", true)]
@@ -29,7 +39,44 @@ namespace HashTableTest
             ht.Add("Dog", new Node("Dog", 27));
 
             Assert.True(ht.Contains(key) ==  found);
-            
+        }
+        
+        /// <summary>
+        /// Tests the hash
+        /// </summary>
+        /// <param name="key">they key to has</param>
+        [Theory]
+        [InlineData("First")]
+        [InlineData("Second")]
+        [InlineData("Third")]
+        public void TestGetHash(string key)
+        {
+            HashTable ht = new HashTable(17);
+            int index = 0;
+
+            for(int i = 0; i < key.Length; i++)
+            {
+                index += (int)key[i];
+            }
+            Assert.True((index * 137) % 17 == ht.GetHash(key));
+        }
+
+        /// <summary>
+        /// Tests the find method
+        /// </summary>
+        /// <param name="key">the key to find</param>
+        /// <param name="found">the expected result</param>
+        [Theory]
+        [InlineData("Cat", true)]
+        [InlineData("Dog", true)]
+        [InlineData("Third", false)]
+        public void TestFindHashTable(string key, bool found)
+        {
+            HashTable ht = new HashTable();
+            ht.Add("Cat", new Node("Cat", 50));
+            ht.Add("Dog", new Node("Dog", 97));
+
+            Assert.True((ht.Find(key) != null) == found);
         }
     }
 }
