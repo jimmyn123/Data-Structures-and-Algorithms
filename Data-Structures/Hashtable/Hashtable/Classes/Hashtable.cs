5407f397ee0865ace39b823b6a06a350c816320c
jimmyn123 <jimmyn123@gmail.com>
2018-11-13 16:06:56 -0800

    added comments

--- Data-Structures/Hashtable/Hashtable/Classes/Hashtable.cs
+++ Data-Structures/Hashtable/Hashtable/Classes/Hashtable.cs
@@ -8,15 +8,27 @@ namespace Hashtable.Classes
     {
         public Node[] HashTableArray { get; set; }
 
+        /// <summary>
+        /// default constructor, defaults to 13 buckets
+        /// </summary>
         public HashTable() : this(13)
         {
         }
 
+        /// <summary>
+        /// Makes a new array with n buckets
+        /// </summary>
+        /// <param name="buckets">the size of array</param>
         public HashTable(int buckets)
         {
             HashTableArray = new Node[buckets];
         }
 
+        /// <summary>
+        /// gets the index by hashing the string
+        /// </summary>
+        /// <param name="key">the key to hash</param>
+        /// <returns>the index</returns>
         public int GetHash(string key)
         {
             int index = 0;
@@ -30,9 +42,16 @@ namespace Hashtable.Classes
             return index;
         }
 
+        /// <summary>
+        /// adds the key value pair to the hashtable
+        /// </summary>
+        /// <param name="key">the string key</param>
+        /// <param name="input">the value</param>
         public void Add(string key, object input)
         {
             int index = GetHash(key);
+
+            // if the bucket is empty, set the first node
             if(HashTableArray[index] == null)
             {
                 HashTableArray[index] = new Node(key, input);
@@ -44,6 +63,7 @@ namespace Hashtable.Classes
 
                 bool uniqueKey = true;
 
+                // iterate through the list, if there is a match, replace the string with new value
                 while(curr != null)
                 {
                     if(curr.Key == key)
@@ -55,6 +75,7 @@ namespace Hashtable.Classes
                     curr = curr.Next;
                 }
 
+                // if it is completely new, add to the end
                 if (uniqueKey)
                 {
                     prev.Next = new Node(key, input);
@@ -62,21 +83,33 @@ namespace Hashtable.Classes
             }
         }
 
+        /// <summary>
+        /// If the hashtable finds the key
+        /// </summary>
+        /// <param name="key">the key to search for</param>
+        /// <returns>if the hashtable contains the key</returns>
         public bool Contains(string key)
         {
             return Find(key) != null;
         }
 
-        public Node Find(string key)
+        /// <summary>
+        /// Finds the key and returns the node
+        /// </summary>
+        /// <param name="key">the key to find</param>
+        /// <returns>the value</returns>
+        public object Find(string key)
         {
             int index = GetHash(key);
 
             Node curr = HashTableArray[index];
+
+            // iterate through the table, return the value if the key is the same
             while (curr != null)
             {
                 if (curr.Key == key)
                 {
-                    return curr;
+                    return curr.Value;
                 }
             }
             return null;
--- Data-Structures/Hashtable/Hashtable/Program.cs
+++ Data-Structures/Hashtable/Hashtable/Program.cs
@@ -8,15 +8,19 @@ namespace Hashtable
     {
         static void Main(string[] args)
         {
+            // create new hash table
             HashTable ht = new HashTable();
 
+            // creates a node
             WriteLine("Created new hashtable....");
             Node initial = new Node("Cat", 15);
 
+            // finds the hash of the string
             WriteLine("Creating new node that has key: Cat and value: 15");
             WriteLine("After hash, Cat should have index: 10");
             WriteLine("Actual index: " + ht.GetHash(initial.Key));
 
+            // adds and tests contains
             ht.Add(initial.Key, initial);
             WriteLine("Contains Cat key: " + ht.Contains("Cat"));
         }
